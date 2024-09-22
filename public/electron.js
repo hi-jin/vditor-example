9dd940da8df5e0208d23e4d2a72b025683c8711a
HyungJin Ahn <sudwlsdks@gmail.com>
2024-09-23 00:48:51 +0900

    initialize electron

---
+++ public/electron.js
@@ -0,0 +1,40 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+let isDev = typeof import("electron-is-dev");
+
+async function createWindow() {
+    isDev = await import("electron-is-dev");
+
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+    });
+
+    mainWindow.loadURL(
+        isDev
+            ? 'http://localhost:3000'
+            : `file://${path.join(__dirname, '../build/index.html')}`
+    );
+
+    if (isDev) {
+        mainWindow.webContents.openDevTools();
+    }
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
